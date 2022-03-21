f7b7e12d76c2cc00d50f8da6b4643fdc02fa53af
dale-q-school <100294005+dale-q-school@users.noreply.github.com>
2022-03-21 15:12:53 -0400

    Main Menu Added

--- main.py
+++ main.py
@@ -15,17 +15,30 @@ while choice != '0':
     choice = input("Choice: ")
     print()
 
+    # Exit
     if choice == '0':
         print("Thank you for playing! Goodbye!")
 
+    # Tic Tac Toe
     elif choice == '1':
         print("Great Choice! Have Fun!")
+        print()
 
+        # Tic Tac Toe code goes here
+
+    # Rock, Paper, Scissors
     elif choice == '2':
         print("Great Choice! Have Fun!")
+        print()
 
+        # Rock, Paper, Scissors code goes here
+
+    # Text Adventure
     elif choice == '3':
         print("Great Choice! Have Fun!")
+        print()
+
+        # Text Adventure code goes here
 
     else:
         print("Invalid choice. Please select again.")
