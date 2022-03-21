d5a586078e88b8e48fc71c9bbd05c47ff097df10
dale-q-school <100294005+dale-q-school@users.noreply.github.com>
2022-03-21 15:08:41 -0400

    Main Menu Added

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-# Code to be done
-# Testing
+# Main Menu
+choice = None
+while choice != '0':
+    print(
+        """
+        Choose a game to play!
+        
+        0 - Quit
+        1 - Tic Tac Toe
+        2 - Rock, Paper, Scissors
+        3 - Text Adventure
+        """
+    )
 
+    choice = input("Choice: ")
+    print()
+
+    if choice == '0':
+        print("Thank you for playing! Goodbye!")
+
+    elif choice == '1':
+        print("Great Choice! Have Fun!")
+
+    elif choice == '2':
+        print("Great Choice! Have Fun!")
+
+    elif choice == '3':
+        print("Great Choice! Have Fun!")
+
+    else:
+        print("Invalid choice. Please select again.")
